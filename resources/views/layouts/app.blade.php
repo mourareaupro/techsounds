9d4298d8286e3e3a1e39141b56f27851b457eb1e
Mathieu <mathieu@iMac-de-Mathieu.local>
2019-01-19 13:29:06 +0100

    fix footer newsletter block to improve

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -120,74 +120,43 @@
         <footer class="footer">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-3">
-                        <h1 class="title"><a class="navbar-brand" href="{{ url('/') }}">
-                                {{ config('app.name', 'Laravel') }}
-                            </a></h1>
+                    <div class="col">
+                        <span>1 of 3</span>
                     </div>
-                    <div class="col-md-3">
-                        <!--<ul class="nav">
-                            <li class="nav-item">
-                                <a href="../index.html" class="nav-link">
-                                    Home
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="../examples/landing-page.html" class="nav-link">
-                                    Landing
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="../examples/register-page.html" class="nav-link">
-                                    Register
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="../examples/profile-page.html" class="nav-link">
-                                    Profile
-                                </a>
-                            </li>
-                        </ul>-->
+                    <div class="col-6">
+
+                        <div id="mc_embed_signup_scroll" class="newsletter-wrap">
+                            <h3 class="text-center">Subscribe to our Newsletter</h3>
+                            <div class="newsletter-wrap-inner"><div class="form-group">
+                                    <div class="mc-field-group "><label for="mce-EMAIL">Email Address </label>
+                                        <input type="email" value="" placeholder="email@mail.com" name="EMAIL" id="mce-EMAIL" class="form-control required email">
+                                    </div>
+                                    <div id="mce-responses" class="clear">
+                                        <div id="mce-error-response" class="response" style="display: none;">
+
+                                        </div>
+                                        <div id="mce-success-response" class="response" style="display: none;">
+
+                                        </div>
+                                    </div>
+                                    <div aria-hidden="true" style="position: absolute; left: -5000px;">
+                                        <input type="text" name="b_c7c1912bd58997935a2b3a33b_a8ffbbc2b0" tabindex="-1" value="">
+                                    </div>
+                                </div>
+                                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary">
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-md-3">
-                        <!--<ul class="nav">
-                            <li class="nav-item">
-                                <a href="https://creative-tim.com/contact-us" class="nav-link">
-                                    Contact Us
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="https://creative-tim.com/about-us" class="nav-link">
-                                    About Us
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="https://creative-tim.com/blog" class="nav-link">
-                                    Blog
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="https://opensource.org/licenses/MIT" class="nav-link">
-                                    License
-                                </a>
-                            </li>
-                        </ul>-->
-                    </div>
-                    <div class="col-md-3">
-                        <!--<h3 class="title">Follow us:</h3>
-                        <div class="btn-wrapper profile">
-                            <a target="_blank" href="https://twitter.com/creativetim" class="btn btn-icon btn-neutral btn-round btn-simple" data-toggle="tooltip" data-original-title="Follow us">
-                                <i class="fab fa-twitter"></i>
-                            </a>
-                            <a target="_blank" href="https://www.facebook.com/creativetim" class="btn btn-icon btn-neutral btn-round btn-simple" data-toggle="tooltip" data-original-title="Like us">
-                                <i class="fab fa-facebook-square"></i>
-                            </a>
-                            <a target="_blank" href="https://dribbble.com/creativetim" class="btn btn-icon btn-neutral  btn-round btn-simple" data-toggle="tooltip" data-original-title="Follow us">
-                                <i class="fab fa-dribbble"></i>
-                            </a>
-                        </div>-->
+                    <div class="col">
+                        <span>3 of 3</span>
                     </div>
                 </div>
+                <!-- Copyright -->
+                <div class="footer-copyright text-center py-3">© 2019 Copyright:
+                    <a href="https://mdbootstrap.com/education/bootstrap/"> techsoundsplus.com</a>
+                </div>
+                <!-- Copyright -->
             </div>
         </footer>
     </div>
