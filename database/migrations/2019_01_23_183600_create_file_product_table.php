821090eea6ef987c007d88dd5ab97cbd9efd5835
mathieu <mourareau.mathieu.pro@gmail.com>
2019-01-23 19:41:17 +0100

    Add file product migration

---
+++ database/migrations/2019_01_23_183600_create_file_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFileProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file_product', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('file_name')->nullable();
+            $table->integer('product_id')->unsigned();
+            $table->integer('path')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file_product');
+    }
+}
